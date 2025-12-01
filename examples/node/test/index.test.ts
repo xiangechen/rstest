057f19b9e0a922474890de38613d8e0106d7a966
xiange <xiangetg@msn.cn>
2025-12-01 15:51:44 +0800

    add TypeError: Converting circular structure to JSON

--- examples/node/test/index.test.ts
+++ examples/node/test/index.test.ts
@@ -1,12 +1,22 @@
 import { describe, expect, it } from '@rstest/core';
-import { sayHi } from '../src/index';
 
-describe('Index', () => {
-  it('should add two numbers correctly', () => {
-    expect(1 + 1).toBe(2);
-  });
+class A {
+  c: C | undefined;
+  constructor(readonly b: B) {}
+}
 
-  it('should test source code correctly', () => {
-    expect(sayHi()).toBe('hi');
+class B {
+  a: A = new A(this);
+}
+
+class C {
+  next: C | undefined;
+  constructor(readonly b: B) {}
+}
+
+describe('test TypeError: Converting circular structure to JSON', () => {
+  it('should throw TypeError: Converting circular structure to JSON', () => {
+    const b = new B();
+    expect(b.a.c?.next).toEqual(new C(b));
   });
 });

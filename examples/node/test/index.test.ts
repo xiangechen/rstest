7e9b8dab141a2ca6aaf7cee436f1dff459de406e
xiange <xiangetg@msn.cn>
2025-12-01 16:02:15 +0800

    remove C

--- examples/node/test/index.test.ts
+++ examples/node/test/index.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, it } from '@rstest/core';
 
 class A {
-  c: C | undefined;
   constructor(readonly b: B) {}
 }
 
@@ -9,14 +8,9 @@ class B {
   a: A = new A(this);
 }
 
-class C {
-  next: C | undefined;
-  constructor(readonly b: B) {}
-}
-
 describe('test TypeError: Converting circular structure to JSON', () => {
   it('should throw TypeError: Converting circular structure to JSON', () => {
     const b = new B();
-    expect(b.a.c?.next).toEqual(new C(b));
+    expect(b.a).toEqual(undefined);
   });
 });
